d1e909e1ef167e38db38843d73f9c5fdcc1dab1e
“wuyong2294508” <276762653@qq.com>
2019-03-07 11:56:32 +0000

    将VideoCapture和打开摄像头操作放在init函数中。现在的帧数已经达到要求， 观看起来很流畅。

--- camera_opencv.py
+++ camera_opencv.py
@@ -1,21 +1,17 @@
 import cv2
 
 class CameraCapture():
-    video_source = 0
+	def __init__(self):
+		self.camera = cv2.VideoCapture(0)
+		if not self.camera.isOpened():
+			raise RuntimeError('Could not start camera.')
 
-    @staticmethod
-    def set_video_source(source):
-        Camera.video_source = source
-
-    @staticmethod
-    def frames():
-        camera = cv2.VideoCapture(CameraCapture.video_source)
-        if not camera.isOpened():
-            raise RuntimeError('Could not start camera.')
-
-        while True:
-            # read current frame
-            _, img = camera.read()
-
-            # encode as a jpeg image and return it
-            return cv2.imencode('.jpg', img)[1].tobytes()
+		
+	def __del__(self):
+		self.camera.release
+		
+	def frames(self):
+		# read current frame
+		_, img = self.camera.read()
+		# encode as a jpeg image and return it
+		return cv2.imencode('.jpg', img)[1].tobytes()
